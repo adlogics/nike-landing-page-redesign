3d93da8b14deacde450883cf2f53b1a65eddf4bf
adlogics <contactanuragnow@gmail.com>
2025-09-23 12:49:58 +0530

    First Draft

---
+++ local.js
@@ -0,0 +1,247 @@
+const navbarOpen = (contentType) => {
+  let output1;
+  if (contentType === "New_Featured") {
+    output1 = `<div class="nav-show">
+                <ul class="menu-show" role="menu">
+                    <h2>Featured</h2>
+                    <li>New & Upcoming Drops</li>
+                    <li>New Arrivals</li>
+                    <li>Bestsellers</li>
+                    <li>SNKRS Launch Calendar</li>
+                    <li>Customise with Nike By You</li>
+                    <li>Jordan</li>
+                    <li>LeBron James</li>
+                </ul>
+                <ul class="menu-show" role="menu">
+                    <h2>Trending</h2>
+                    <li>Summer Essentials</li>
+                    <li>Structure 26 - Run Supported</li>
+                    <li>What's Trending</li>
+                    <li>Retro Running</li>
+                    <li>Running Shoe Finder</li>
+                </ul>
+                <ul class="menu-show" role="menu">
+                    <h2>Shop Icons</h2>
+                    <li>Lifestyle</li>
+                    <li>Air Force 1</li>
+                    <li>Air Jordan 1</li>
+                    <li>Air Max</li>
+                    <li>Dunk</li>
+                    <li>Cortez</li>
+                    <li>Blazer</li>
+                    <li>Pegasus</li>
+                    <li>Vomero</li>
+                </ul><ul class="menu-show" role="menu">
+                    <h2>Shop by sports</h2>
+                    <li>Running</li>
+                    <li>Basketball</li>
+                    <li>Football</li>
+                    <li>Golf</li>
+                    <li>Tennis</li>
+                    <li>Gym and Training</li>
+                    <li>Yoga</li>
+                    <li>Skateboarding</li>
+                </ul>
+            </div>`;
+  } else if (contentType === "Men") {
+    output1 = `<div class="nav-show">
+                <ul class="menu-show" role="menu">
+                    <h2>Featured</h2>
+                    <li>New Arrivals</li>
+                    <li>Bestsellers</li>
+                    <li>Shop All Sale</li>
+                </ul>
+                <ul class="menu-show" role="menu">
+                    <h2>Shoes</h2>
+                    <li>All Shoes</li>
+                    <li>Lifestyle</li>
+                    <li>Jordan</li>
+                    <li>Running</li>
+                    <li>Football</li>
+                    <li>Basketball</li>
+                    <li>Gym and Training</li>
+                    <li>Skateboarding</li>
+                    <li>Sandals and Slides</li>
+                    <li>Nike By You</li>                  
+                </ul>
+                <ul class="menu-show" role="menu">
+                    <h2>Clothing</h2>
+                    <li>All Clothing</li>
+                    <li>Tops and T-Shirts</li>
+                    <li>Shorts</li>
+                    <li>Pants and Leggings</li>
+                    <li>Hoodies and Sweatshirts</li>
+                    <li>Jackets and Gilets</li>
+                    <li>Jerseys and Kits</li>
+                    <li>Jordan</li>
+                </ul><ul class="menu-show" role="menu">
+                    <h2>Shop by sports</h2>
+                    <li>Running</li>
+                    <li>Basketball</li>
+                    <li>Football</li>
+                    <li>Golf</li>
+                    <li>Tennis</li>
+                    <li>Gym and Training</li>
+                    <li>Yoga</li>
+                    <li>Skateboarding</li>                  
+                </ul>
+                </ul><ul class="menu-show" role="menu">
+                    <h2>Accessories and Equipment</h2>
+                    <li>All Accessories and Equipment</li>
+                    <li>Bags and Backpacks</li>
+                    <li>Socks</li>
+                    <li>Hats and Headwear</li>               
+                </ul>
+            </div>`;
+  } else if (contentType === "Women") {
+    output1 = `<div class="nav-show">
+                <ul class="menu-show" role="menu">
+                    <h2>Featured</h2>
+                    <li>New Arrivals</li>
+                    <li>Bestsellers</li>
+                    <li>Shop All Sale</li>
+                </ul>
+                <ul class="menu-show" role="menu">
+                    <h2>Shoes</h2>
+                    <li>All Shoes</li>
+                    <li>Lifestyle</li>
+                    <li>Jordan</li>
+                    <li>Running</li>
+                    <li>Gym and Training</li>
+                    <li>Football</li>
+                    <li>Basketball</li>
+                    <li>Sandals and Slides</li>
+                    <li>Nike By You</li>
+                </ul>
+                <ul class="menu-show" role="menu">
+                    <h2>Clothing</h2>
+                    <li>All Clothing</li>
+                    <li>Performance Essentials</li>
+                    <li>Tops and T-Shirts</li>
+                    <li>Sports Bras</li>
+                    <li>Pants and Leggings</li>
+                    <li>Shorts</li>
+                    <li>Skirts and Dresses</li>
+                    <li>Modest Wear</li>
+                    <li>Nike Maternity</li>
+                    <li>Plus Size</li>    
+                </ul><ul class="menu-show" role="menu">
+                    <h2>Shop by sports</h2>
+                    <li>Yoga</li>
+                    <li>Running</li>
+                    <li>Gym and Training</li>
+                    <li>Basketball</li>
+                    <li>Tennis</li>
+                    <li>Golf</li>
+                    <li>Football</li>
+                    <li>Skateboarding</li>
+                </ul>
+                </ul><ul class="menu-show" role="menu">
+                    <h2>Accessories and Equipment</h2>
+                    <li>All Accessories and Equipment</li>
+                    <li>Bags and Backpacks</li>
+                    <li>Socks</li>
+                    <li>Hats and Headwear</li>               
+                </ul>
+            </div>`;
+  } else if (contentType === "Kids") {
+    output1 = `<div class="nav-show">
+                <ul class="menu-show" role="menu">
+                    <h2>Featured</h2>
+                    <li>New Arrivals</li>
+                    <li>Bestsellers</li>
+                    <li>Back to School</li>
+                    <li>Sport Gear</li>
+                    <li>Lifestyle Looks</li>
+                </ul>
+                <ul class="menu-show" role="menu">
+                    <h2>Shoes</h2>
+                    <li>All Shoes</li>
+                    <li>Lifestyle</li>
+                    <li>Jordan</li>
+                    <li>Football</li>
+                    <li>Running</li>
+                    <li>Basketball</li>
+                </ul>
+                <ul class="menu-show" role="menu">
+                    <h2>Clothing</h2>
+                    <li>All Clothing</li>
+                    <li>Tops and T-Shirts</li>
+                    <li>Sport Bras</li>
+                    <li>Hoodies and Sweatshirts</li>
+                    <li>Pants and Leggings</li>
+                    <li>Shorts</li>
+                    <li>Jackets and Gilets</li>                       
+                </ul><ul class="menu-show" role="menu">
+                    <h2>Kids By Age</h2>
+                    <li>Older Kids (7 - 14 years)</li>
+                    <li>Younger Kids (4 - 7 years)</li>
+                    <li>Babies & Toddlers (0 - 4 years)</li>
+                </ul>
+                </ul><ul class="menu-show" role="menu">
+                    <h2>Accessories and Equipment</h2>
+                    <li>All Accessories and Equipment</li>
+                    <li>Bags and Backpacks</li>
+                    <li>Socks</li>
+                    <li>Hats and Headwear</li>               
+                </ul>
+            </div>`;
+  }
+  const dropdown = document.getElementById("showit");
+  dropdown.innerHTML = output1;
+  dropdown.style.display = "block";
+};
+
+const navbarClose = () => {
+  document.getElementById("showit").innerHTML = "";
+};
+
+
+let imgIndex = 0;
+const imageschange = ["./images/shoe1-nike-just-do-it-removebg.png", "./images/shoe2-nike-just-do-it-removebg-preview.png", "./images/shoe3-nike-just-do-it-removebg-preview.png" ]
+
+const imgElement = document.getElementById('mid-images');
+function changeImage() {
+  imgIndex = (imgIndex + 1) % imageschange.length;
+//   imgElement.style.opacity = 0;
+  setTimeout(() => {
+    imgElement.src = imageschange[imgIndex];  // Update the image source
+    // imgElement.style.opacity = 1;
+    }, 1000);
+}
+setInterval(changeImage,1000)
+
+
+// productssss
+let productListHTML = document.getElementById("products-container");
+let productList = [];
+
+const addDataToHTML= ()=>{
+    productListHTML.innerHTML = "";
+    if(productList.length>0){
+        productList.forEach(product=>{
+            let newProduct = document.createElement("div");
+            newProduct.classList.add('product-card');
+            newProduct.dataset.id = product.id;
+            newProduct.innerHTML = 
+            `<img src="${product.image}" alt="product1">
+              <div class="c-txt">
+                <span>${product.name}</span>
+                <h3>MRP : ₹ ${product.price}</h3>
+              </div>`
+            productListHTML.appendChild(newProduct);
+        })
+    }
+};
+
+
+const initApp = ()=>{
+    fetch('products.json')
+    .then(response=>response.json())
+    .then(data=>{
+        productList = data
+        addDataToHTML();
+        
+    })
+}
+initApp();
